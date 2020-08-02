60a22d12f99df1bb80def9d62d85622254c0e2f2
rafifihdhar <60249761+rafifihdhar@users.noreply.github.com>
2020-08-02 23:12:49 +0700

    database update

--- dblabfik/tampilan.sql
+++ dblabfik/tampilan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 26, 2020 at 01:33 PM
+-- Generation Time: Aug 02, 2020 at 06:11 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tampilan` (
   `id_tampilan` int(11) NOT NULL,
-  `id` int(11) NOT NULL,
+  `id` varchar(64) NOT NULL,
   `slug_tampilan` varchar(255) NOT NULL,
   `id_kategori` int(11) NOT NULL,
   `id_ck` int(11) NOT NULL,
@@ -37,11 +37,8 @@ CREATE TABLE `tampilan` (
   `tanggal_post` datetime NOT NULL,
   `tanggal_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `views` int(11) NOT NULL DEFAULT 0,
-  `nim` int(20) NOT NULL,
-  `kode_tampilan` varchar(255) NOT NULL,
   `judul` varchar(30) NOT NULL,
   `deskripsi` text NOT NULL,
-  `keywords` varchar(255) NOT NULL,
   `likes` int(11) NOT NULL DEFAULT 0,
   `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -50,9 +47,10 @@ CREATE TABLE `tampilan` (
 -- Dumping data for table `tampilan`
 --
 
-INSERT INTO `tampilan` (`id_tampilan`, `id`, `slug_tampilan`, `id_kategori`, `id_ck`, `gambar`, `tanggal_post`, `tanggal_update`, `views`, `nim`, `kode_tampilan`, `judul`, `deskripsi`, `keywords`, `likes`, `status`) VALUES
-(3, 44, 'cyberpunk-001', 7, 1, '1238334.jpg', '2020-07-26 17:43:19', '2020-07-26 10:55:19', 1, 1301196969, '001', 'cyberpunk', 'calon game terbaik tahun 2020', 'punk', 1, 'Diterima'),
-(4, 38, 'witcher-002', 7, 1, '2226191.jpg', '2020-07-26 17:45:48', '2020-07-26 10:47:42', 1, 1301121313, '002', 'witcher', 'game of the year 2017', 'witch', 0, 'Diterima');
+INSERT INTO `tampilan` (`id_tampilan`, `id`, `slug_tampilan`, `id_kategori`, `id_ck`, `gambar`, `tanggal_post`, `tanggal_update`, `views`, `judul`, `deskripsi`, `likes`, `status`) VALUES
+(3, '44', 'cyberpunk-001', 7, 1, '1238334.jpg', '2020-07-26 17:43:19', '2020-07-26 10:55:19', 1, 'cyberpunk', 'calon game terbaik tahun 2020', 1, 'Diterima'),
+(4, '38', 'desain-fashion', 6, 2, 'images.jpg', '2020-07-26 17:45:48', '2020-08-02 15:38:14', 1, 'desain fashion', 'fashion master', 0, 'Diterima'),
+(5, '44', 'pringles', 2, 4, 'download_(3).jpg', '2020-08-02 22:21:52', '2020-08-02 15:21:52', 0, 'pringles', 'project pembuatan desain produk kentang pringles', 0, 'Menunggu Acc');
 
 --
 -- Indexes for dumped tables
@@ -72,7 +70,7 @@ ALTER TABLE `tampilan`
 -- AUTO_INCREMENT for table `tampilan`
 --
 ALTER TABLE `tampilan`
-  MODIFY `id_tampilan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_tampilan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- dblabfik/user.sql
+++ dblabfik/user.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 27, 2020 at 08:59 AM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.3.12
+-- Generation Time: Aug 02, 2020 at 06:12 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,10 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `user` (
   `id` varchar(64) NOT NULL,
-  `booking_id` varchar(64) NOT NULL,
-  `borrowing_id` varchar(64) NOT NULL,
   `username` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `nim` varchar(11) NOT NULL,
+  `prodi` varchar(64) NOT NULL,
   `email` varchar(255) NOT NULL,
   `images` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -48,13 +47,13 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `booking_id`, `borrowing_id`, `username`, `name`, `email`, `images`, `password`, `salt`, `role_id`, `is_active`, `date_created`, `status`) VALUES
-('38', '', '', 'rochieko', 'Rochi Eko Pambudi', 'snowm60401@gmail.com', 'default.jpg', 'a4dd82e95096136291c033406a6b25f598b0eb0e0444b554706b926690bbc16f', '$2y$10$zBadfaR3yj3UEYWGaFLafOUv5uP6UkMBy691b9a54fjBhKDFQ8G7q', 3, 1, 0, '0'),
-('39', '', '', 'kaurlab', 'Kaur Lab ', 'kaurlab@gmail.com', 'default.jpg', '920c3713e13b091e73d17d35bd608079fc41724eca41b415f200e338dc59c531', '$2y$10$hctmRhwo9qxeJTvtzbn/kObWapiE8JSPX6jO72QAbp1HJfe4QBwEi', 2, 1, 0, '0'),
-('44', '', '', 'jhondoe', 'Jhon Doe Version 2', 'snowm60401@gmail.com', 'default.jpg', 'e41e13ea4344a5dab62674d6e08a24b75bf0d5bd7921c04c2a13fc80a6eda0e3', '$2y$10$sGYdQGJYGX9nCIDzkWoH3uibGxPC292Bf9nhIgO/TSkLz3Q3Sp1jO', 4, 1, 0, '0'),
-('5f140497dd7ef', '', '', 'sulthan', 'Sulthan Angka Kurniawan', 'sulthan@gmail.com', 'default.jpg', '6b231a475b44310a9acf574b6a9c2ad9562d5c3a02ea08b8353c626a4a22ba38', '$2y$10$08qhkiZqoPdw1LRp4ujk9umYIOaBbrtQ0k//iYABmlstVCOVPVu6e', 4, 1, 0, 'offline'),
-('5f1b94cd297bc', '', '', 'snowm22', 'Snow qsas', 'snowm604021@gmail.com', 'default.jpg', '874c1a418291ecb0fb093b11f116f858519d756d45d96e3af62448d1eee56ac6', '$2y$10$.3U5ExKBFd78E.z//O2kOOhrMFA7tJUN5hLFbmusrCTQmCRVN/DOi', 4, 1, 1595643085, 'offline'),
-('8', '', '', 'admin', 'John Doe', 'admin@gmail.com', 'default.jpg', 'ec54193c7b13f115a35da3282d74a295af9a72ca8f8a5ebd9655dbf8eadd8a02', '$2y$10$jb3uBvvS41mfsMHU4xaICul08WsrJzMyLpiIVT9bpx06CQQ/vmNle', 1, 1, 0, '0');
+INSERT INTO `user` (`id`, `username`, `name`, `nim`, `prodi`, `email`, `images`, `password`, `salt`, `role_id`, `is_active`, `date_created`, `status`) VALUES
+('38', 'rochieko', 'Rochi Eko Pambudi', '', '', 'snowm60401@gmail.com', 'default.jpg', 'a4dd82e95096136291c033406a6b25f598b0eb0e0444b554706b926690bbc16f', '$2y$10$zBadfaR3yj3UEYWGaFLafOUv5uP6UkMBy691b9a54fjBhKDFQ8G7q', 3, 1, 1594543149, 'offline'),
+('39', 'kaurlab', 'Kaur Lab ', '', '', 'kaurlab@gmail.com', 'default.jpg', '920c3713e13b091e73d17d35bd608079fc41724eca41b415f200e338dc59c531', '$2y$10$hctmRhwo9qxeJTvtzbn/kObWapiE8JSPX6jO72QAbp1HJfe4QBwEi', 2, 1, 1594554238, 'offline'),
+('44', 'ihdar', 'Rafif ihdhar', '1301174633', 'DKV', 'snowm6040@gmail.com', '01-avatars.jpg', 'e41e13ea4344a5dab62674d6e08a24b75bf0d5bd7921c04c2a13fc80a6eda0e3', '$2y$10$sGYdQGJYGX9nCIDzkWoH3uibGxPC292Bf9nhIgO/TSkLz3Q3Sp1jO', 4, 1, 1594832402, 'online'),
+('5f1e7dc5ca07e', 'sulthanangka', 'Muhammad Sulthan Angka Kurniawan', '', '', 'sulthan.kurniawan@gmail.com', 'default.jpg', '7e93fd68a7b5f0860784f35336a488910b3d6f2c088602a4a608e24ebeac3a36', '$2y$10$IXEl6J4l/ORTrf78B14hyewCsBz1Fyf4xM96cQPexqL.KqvJ4A2zC', 4, 1, 1595833797, 'online'),
+('5f2128a43c90b', 'akathan', 'akathan', '', '', 'sulthan.angka@gmail.com', 'default.jpg', '95b77ac94e00b2039b79d78e01ee5f941da1d074fae0a3a41636797e429bd860', '$2y$10$UdpWt4Uo/v1rlkzJxZqrdu7mlLiJbI3aRrmToglyIduVaYAsL7diG', 4, 1, 1596008612, 'offline'),
+('8', 'admin', 'John Doe', '', '', 'admin@gmail.com', 'default.jpg', 'ec54193c7b13f115a35da3282d74a295af9a72ca8f8a5ebd9655dbf8eadd8a02', '$2y$10$jb3uBvvS41mfsMHU4xaICul08WsrJzMyLpiIVT9bpx06CQQ/vmNle', 1, 1, 0, 'offline');
 
 --
 -- Indexes for dumped tables
@@ -64,9 +63,7 @@ INSERT INTO `user` (`id`, `booking_id`, `borrowing_id`, `username`, `name`, `ema
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `booking_id` (`booking_id`),
-  ADD KEY `borrow_id` (`borrowing_id`);
+  ADD PRIMARY KEY (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
