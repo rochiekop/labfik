de2825b13d80efbee3e37d4646c59c1d2f143864
rafifihdhar <60249761+rafifihdhar@users.noreply.github.com>
2020-08-30 09:51:03 +0700

    dosen wali

--- application/views/dashboard/users/daftarfile.php
+++ application/views/dashboard/users/daftarfile.php
@@ -41,7 +41,7 @@
                             <td><?= $t['nama'] ?></td>
                             <td> <a href="<?= base_url('assets/upload/thesis/') . $t['username'] . '/' . $t['file'] ?>" download title="Download File"><?= $t['file'] ?></a></td>
                             <td><a data-toggle="modal" data-target="#pdf<?= encrypt_url($t['id']); ?>" class="btn badge badge-secondary" style="color: white;">Lihat</a></td>
-                            <?php if ($t['status_doswal'] == "Dikirim" or $t['status_doswal'] == "Update File") : ?>
+                            <?php if ($t['status_doswal'] == "Dikirim" or $t['status_doswal'] == "Update file") : ?>
                                 <td> <a href="<?= base_url('users/accta/') . encrypt_url($t['id']) ?>" class="btn badge badge-success">Acc</a>
                                     <a data-toggle="modal" data-target="#<?= encrypt_url($t['id']); ?>" class="btn badge badge-danger" style="color: white;">Tolak</a>
                                 </td>
@@ -51,7 +51,7 @@
                                 <td></td>
                                 <td><b>Acc</b></td>
                                 <td></td>
-                            <?php elseif ($t['status_doswal'] == "Ditolak") : ?>
+                            <?php elseif ($t['status_doswal'] == "Ditolak wali") : ?>
                                 <td><a data-toggle="modal" data-target="#komen<?= encrypt_url($t['id']); ?>" class="badge badge-warning" style="color:white">Beri Tanggapan</a></td>
                                 <td><b>Tolak</b></td>
                                 <td><?= $t['komentar'] ?></td>
