fd65d3f6cd737f9d56abceca860cb7fe97fb9f87
Rochi Eko Pambudi <rochieko11@gmail.com>
2020-09-03 21:21:46 +0700

    hapus thesis_lecturers

--- application/views/dashboard/users/pendaftarantugasakhir.php
+++ application/views/dashboard/users/pendaftarantugasakhir.php
@@ -7,8 +7,6 @@
     <?php if ($statusfile == null) : ?>
     <?php elseif ($statusfile == "Disetujui Adminlaa" and empty($thesis_lecturers)) : ?>
       <div class="alert alert-warning">Selamat! pengajuan TA anda sudah disetujui oleh semua pihak, silakan tunggu <b>2x24 jam</b> untuk Koordinator TA memberikan dosen pembimbing anda, Terima kasih. <br> <a href="<?= base_url('Chat/getAllKoordinatorTA') ?>" class="btn btn-primary btn-sm" style="margin-top:6px;">Hubungi Koordinator TA</a> </div>
-    <?php elseif (!empty($thesis_lecturers)) : ?>
-      <div class="alert alert-success">Koordinator TA telah menambahkan <?= $dosbing1['name'] ?> dan <?= $dosbing2['name'] ?> sebagai dosen pembimbing tugas akhir anda,<br> <a href="<?= base_url('users/bimbingantugasakhir') ?>" class="btn btn-success btn-sm" style="margin-top:6px;">Mulai Bimbingan</a> </div>
     <?php endif; ?>
     <?php if (empty($cek)) : ?>
       <a data-toggle="modal" data-target="#judul" class="btn btn-sm btn-primary" style="color:#fff">Daftar Tugas Akhir</a>
